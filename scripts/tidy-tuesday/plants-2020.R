f5683e01466de37f3a6e84d5ab99fa5c03515951
Roxana <noelia0618@gmail.com>
2020-08-19 02:11:00 -0300

    threats

--- scripts/tidy-tuesday/plants-2020.R
+++ scripts/tidy-tuesday/plants-2020.R
@@ -9,6 +9,8 @@ glimpse(plants)
 glimpse(actions)
 glimpse(threats)
 
+library(tidytext)
+library(tidyverse)
 #plantas extintas por continente
 plants %>%
   ggplot(aes(x=red_list_category, fill=group))+
@@ -22,7 +24,7 @@ plants %>%
 
 plants %>%
   count(group, sort=TRUE)
-
+#paises que presentan mayor amenaza para especies de plantas
 plants %>%
   count(country=fct_lump(country,20), sort=TRUE)%>%
   filter(country!="Other")%>%
@@ -33,6 +35,53 @@ plants %>%
        subtitle="tidytuesday 18-08-2020",
        caption="@data-datum")
 
-plants%>%
-  count(fct_lump(country, 15), sort=TRUE)
+#tipos de amenazas segun continente
+threats %>%
+  count(continent, sort=TRUE)
+threats %>%
+  ggplot(aes(threat_type, fill=red_list_category))+
+  geom_bar()+
+  facet_wrap(~continent)
+#tipos de amenazas segun continente
+threats %>%
+  filter(threatened==1)%>%
+  count(threat_type, continent, sort=TRUE)%>%
+  mutate(threat_type=fct_reorder(threat_type, n))%>%
+  ggplot(aes(x=n, y=threat_type, fill=continent))+
+  geom_col()
+#amenaza en cada continente
+#codigo de david silver tidytuesday screencast
+by_continent_threat<-threats %>%
+  filter(threatened==1)%>%
+  count(threat_type, continent, sort=TRUE)%>%
+  mutate(threat_type=fct_reorder(threat_type, n, sum))
+
+by_continent_threat %>%
+  ggplot(aes(x=n, y=threat_type))+
+  geom_col()+
+  facet_wrap(~continent, scales="free")
+
+by_continent_threat %>%
+  mutate(threat_type=reorder_within(threat_type, n, continent))%>%
+  ggplot(aes(x=n, y=threat_type))+
+  geom_col()+
+  facet_wrap(~continent, scales="free")+
+  labs(title="The Most Common Threats",
+       x="# of plants facing this threat",
+       y="", 
+       fill="Continent")
+
+
+
+
+
+--------------------#notas--------------------------------------------
+#no olvidar la variable q se va a adicionar en count(), MUY IMPORTANTE
+#fct_reorder se utiliza en mutate(), no en ggplot()
+#libreria tidytext tiene la function reorder_within()
+#scales=free en el facetado permite una mejor visualizacion
+#
+
+
+
 
