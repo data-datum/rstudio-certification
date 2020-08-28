f1fb8c0f69f0876c0a9ad811de2bf52cc402a173
Roxana <noelia0618@gmail.com>
2020-08-28 02:20:58 -0300

    graficos

--- scripts/tidy-tuesday/tv_series_2019.R
+++ scripts/tidy-tuesday/tv_series_2019.R
@@ -8,10 +8,28 @@ tv_series %>%
   filter(temporadas>10)%>%
   ggplot(aes(y=title, x=temporadas))+
   geom_col()
+#como se distribuye el puntaje en los distintos generos
+tv_pivoted <-tv_series %>%
+  separate_rows(genres, sep=",")
 
-tv_series %>%
-  count(title, av_rating, sort=TRUE)
+tv_pivoted %>%
+  ggplot(aes(x=av_rating, y=genres))+
+  geom_boxplot()
 
+#cuantas series hay por genero
+tv_pivoted %>%
+  count(genres, sort=TRUE)
 
+#grafico de barras por genero y pintado por año
+tv_pivoted %>%
+  ggplot(aes(x=genres, fill=date))+
+  geom_bar()+
+  coord_flip()
 
-  
+by_year <-tv_pivoted %>%
+  separate(date, sep="-", into = c("year", "month", "day"))
+by_year %>%
+  ggplot(aes(x=av_rating, y=year))+
+  geom_boxplot()+
+  facet_wrap(~genres)
+
